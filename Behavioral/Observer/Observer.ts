93e88470cedde28a1890262689cf2a076bade7f5
Catalin Rusu <catalin112224@gmail.com>
2023-05-18 10:03:55 +0300

    update observer

--- Behavioral/Observer/Observer.ts
+++ Behavioral/Observer/Observer.ts
@@ -1,18 +1,17 @@
-// Definim interfata pentru observatori
 interface Observer {
     update(event: string, data: any): void;
 }
 
-// Definim clasa subiect (Subject) care va notifica observatorii
+
 class ProductManagementSystem {
     private observers: Observer[] = [];
 
-    // Metoda pentru adăugarea unui observator
+
     addObserver(observer: Observer): void {
         this.observers.push(observer);
     }
 
-    // Metoda pentru ștergerea unui observator
+
     removeObserver(observer: Observer): void {
         const index = this.observers.indexOf(observer);
         if (index !== -1) {
@@ -20,39 +19,34 @@ class ProductManagementSystem {
         }
     }
 
-    // Metoda pentru notificarea observatorilor
+
     notifyObservers(event: string, data: any): void {
         for (const observer of this.observers) {
             observer.update(event, data);
         }
     }
 
-    // Metoda pentru adăugarea unui produs
-    addProduct(product: any): void {
-        // Logica de adăugare a produsului
 
-        // Notificăm observatorii că s-a adăugat un produs nou
+    addProduct(product: any): void {
+
+
+
         this.notifyObservers('product_added', product);
     }
 
-    // Metoda pentru actualizarea unui produs
-    updateProduct(product: any): void {
-        // Logica de actualizare a produsului
 
-        // Notificăm observatorii că s-a actualizat un produs
+    updateProduct(product: any): void {
+
         this.notifyObservers('product_updated', product);
     }
 
-    // Metoda pentru ștergerea unui produs
-    deleteProduct(productId: string): void {
-        // Logica de ștergere a produsului
 
-        // Notificăm observatorii că s-a șters un produs
+    deleteProduct(productId: string): void {
+
         this.notifyObservers('product_deleted', productId);
     }
 }
 
-// Implementăm clasa de observator (Observer)
 class UserNotification implements Observer {
     private username: string;
 
@@ -81,22 +75,22 @@ class UserNotification implements Observer {
     }
 }
 
-// Exemplu de utilizare
+
 const productManagementSystem = new ProductManagementSystem();
 
-// Creăm observatori
+
 const user1 = new UserNotification('John');
 const user2 = new UserNotification('Alice');
 
-// Adăugăm observatorii în sistemul de gestiune a produselor
+
 productManagementSystem.addObserver(user1);
 productManagementSystem.addObserver(user2);
 
-// Adăugăm un produs nou
+
 const newProduct = { id: '123', name: 'Laptop', price: 2000 };
 productManagementSystem.addProduct(newProduct);
 
-// Actualizăm un produs existent
+
 const updatedProduct = { id: '456', name: 'Telefon', price: 1500 };
 productManagementSystem.updateProduct(updatedProduct);
 
